b49da81350da70efae7033987917b89913b39d77
Adelita Esparza <aesparza051@gmail.com>
2019-07-22 14:41:24 -0500

    updated gitignore

--- src/main/java/com/compay/GameStoreProject/Service/GameService.java
+++ src/main/java/com/compay/GameStoreProject/Service/GameService.java
@@ -1,4 +1,13 @@
 package com.compay.GameStoreProject.Service;
 
+import com.compay.GameStoreProject.Repositories.GameRepository;
+import com.compay.GameStoreProject.Repositories.Games;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class GameService {
+@Autowired
+    private GameRepository gameRepo;
+public Games add
 }
