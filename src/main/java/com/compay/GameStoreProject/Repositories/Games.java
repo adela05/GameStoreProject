7d42bf37f9ad3cecba1129fff3f2eff43fef4201
Rosa G <51469712+romagarc14@users.noreply.github.com>
2019-07-22 14:35:02 -0500

    added to classes and repos

--- src/main/java/com/compay/GameStoreProject/Repositories/Games.java
+++ src/main/java/com/compay/GameStoreProject/Repositories/Games.java
@@ -9,24 +9,69 @@ import javax.validation.constraints.NotNull;
 
 @Entity
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
-@Table(name="games")
+@Table(name="game")
 public class Games {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Length(max = 11)
     private Integer gameId;
     @NotEmpty
-    private String studio;
-    @NotEmpty
-    private Character eSRBRating;  // Everyone, Teen, Mature, Adult
-    @NotEmpty
+    @Length(max = 50)
     private String title;
+    @NotEmpty
+    @Length(max = 50)
+    private String eSRBRating;  // Everyone, Teen, Mature, Adult
+    @NotEmpty
+    private String description;
+    @NotNull
+    @Length(max = 5)
+    private Double price;
+    @NotEmpty
+    @Length(max = 50)
+    private String studio;
+    @NotNull
+    @Length(max = 11)
+    private Integer quantity;
 
+    // Getters and Setters
     public Integer getGameId() {
         return gameId;
     }
 
     public void setGameId(Integer gameId) {
         this.gameId = gameId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getESRBRating() {
+        return eSRBRating;
+    }
+
+    public void setESRBRating(String eSRBRating) {
+        this.eSRBRating = eSRBRating;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
     }
 
     public String getStudio() {
@@ -37,19 +82,11 @@ public class Games {
         this.studio = studio;
     }
 
-    public Character getESRBRating() {
-        return eSRBRating;
+    public Integer getQuantity() {
+        return quantity;
     }
 
-    public void setESRBRating(Character eSRBRating) {
-        this.eSRBRating = eSRBRating;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
     }
 }
