0419a9b870dd8d06d7440db5cb35db40226d56fc
Rosa G <51469712+romagarc14@users.noreply.github.com>
2019-07-24 13:16:39 -0500

    added repos

--- src/main/java/com/compay/GameStoreProject/Repositories/ProcessingFeeRepository.java
+++ src/main/java/com/compay/GameStoreProject/Repositories/ProcessingFeeRepository.java
@@ -2,5 +2,8 @@ package com.compay.GameStoreProject.Repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProcessingFeeRepository extends JpaRepository<ProcessingFee, Double> {
+    List<ProcessingFee> findByFee(Double fee);
 }
